0419ecec498589b9983f104d37d75716ab983965
Pontus Ekdahl <pontus.ekdahl@gmail.com>
2023-06-14 18:14:02 +0200

    Added ConflictExceptionResponse class

---
+++ Exceptions/ExceptionResponses/ConflictExceptionResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_api_test.Exceptions.ExceptionResponses;
+
+public class ConflictExceptionResponse : HttpExceptionResponse
+{
+    public ConflictExceptionResponse(string msg, int statusCode = StatusCodes.Status409Conflict) : base(statusCode, msg)
+    {
+        StatusCode = statusCode;
+    }
+}
